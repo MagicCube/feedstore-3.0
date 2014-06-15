e43be8880eb93870a7384ae4e402c10535054cdf
Henry Li <henry1943@163.com>
2014-06-15 13:17:46 +0800

    Change Module to Assembly.

--- magiccube-feedstore-server/app.js
+++ magiccube-feedstore-server/app.js
@@ -1,68 +1,18 @@
 var path = require("path");
 var mx = require("mxframework");
 
-
-
-
-
-var express = require("express"),
-    http = require("http"),
-    path = require("path");
-
-var app = express();
-
-console.log("\033[2J");
-console.log("***************************************************************************");
-console.log(" MagicCube FeedStore 3.0 (%s)", app.get("env"));
-console.log("***************************************************************************");
-
-
-
-
-
-// Setup environments.
-app.set("port", process.env.PORT || 18080);
-app.use(express.favicon());
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, "public")));
-
-// Setup routes.
-var routes = require("./routes");
-routes.applyAll(app, [
-    "/api/channels"
-]);
-
-
-// For development only.
-if ("development" == app.get("env"))
-{
-    app.use(express.errorHandler());
-}
-
-
-
-
-
-
-
-
-
-/**
- * Module fss
- */
 $module("fss", path.resolve(module.paths[0], "../lib/fss"));
-require("./settings.js");
-$import("fss.server");
+$import("fss.Server");
 
-// Initialize Server
 fss.server.init();
-fss.server.start(function()
+fss.server.start(function(p_error, p_result)
 {
-    // Start HTTP server.
-    http.createServer(app).listen(app.get("port"), function()
+    if (isEmpty(p_error))
     {
-        mx.logger.info("HTTP server start to listen at " + app.get("port") + ".");
-    });
+        fss.server.run();
+    }
+    else
+    {
+        process.exit();
+    }
 });
--- magiccube-feedstore-server/lib/fss/Server.js
+++ magiccube-feedstore-server/lib/fss/Server.js
@@ -1,6 +1,10 @@
 $ns("fss");
 
 var async = require("async");
+var express = require("express");
+var http = require("http");
+var path = require("path");
+
 
 $import("fss.db.DbConnection");
 $import("fss.model.Channel");
@@ -12,47 +16,157 @@ fss.Server = function()
     me.autoInit = false;
     var base = {};
     
-    me.channelManager = null;
+    /*
+     * development / production
+     */
+    me.runningMode = null;
+    
+    /*
+     * local / bae
+     */
+    me.runat = null;
+    
+    /*
+     * null,
+     * initializing,
+     * initialized,
+     * starting,
+     * started,
+     * preparing
+     * running,
+     * error
+     */
     me.status = null;
+    
+    me.expressApp = null;
+    me.channelManager = null;
     
     base.init = me.init;
     me.init = function(p_options)
     {
         base.init(p_options);
         
-        me.status = "initializing";
-        mx.logger.info("FeedStore server is now initializing...");
+        me.runningMode = process.env.NODE_ENV ? process.env.NODE_ENV : "development";
+        me.runat = process.env.RUNAT ? process.env.RUNAT : "local";
+        
+        _printHeader();
+        
+        _setStatus("initializing");
+        
+        _loadSettings();
         
         // Connect to MongoDB
         fss.db.DbConnection.connect();
         
         // Initialize managers
         me.channelManager = new fss.biz.ChannelManager();
+        
+        _setStatus("initialized");
     };
     
     me.start = function(p_callback)
     {
-        me.status = "starting";
-        mx.logger.info("FeedStore server is now starting...");
+        _setStatus("starting");
         
         async.series([
-            me.channelManager.load
+            me.channelManager.load,
+            _initExpressApp
         ], function(p_err, p_results)
         {
             if (isEmpty(p_err))
             {
-                me.status = "running";
-                mx.logger.info("FeedStore server status is now switch to RUNNING.");
+                _setStatus("started");
             }
             else
             {
-                me.status = "error";
                 mx.logger.error(p_err);
+                _setStatus("error");
             }
             
             p_callback(p_err, p_results);
         });
     };
+    
+    me.run = function(p_callback)
+    {        
+        _setStatus("preparing");
+        _startHttpServer(function()
+        {
+            _setStatus("running");
+            
+            if (isFunction(p_callback))
+            {
+                p_callback();
+            }
+        });
+    };
+    
+    
+    
+    
+    function _initExpressApp()
+    {
+        var app = express();
+        app.set("port", process.env.PORT || 18080);
+        app.use(express.favicon());
+        app.use(express.bodyParser());
+        app.use(express.methodOverride());
+        app.use(app.router);
+        app.use(express.static(path.join(__dirname, "public")));
+        if ("development" == me.runningMode)
+        {
+            app.use(express.errorHandler());
+        }
+        _initRoutes(app);
+        me.expressApp = app;
+    }
+    
+    function _initRoutes(p_app)
+    {
+        var routes = require("./routes");
+        routes.applyAll(p_app, [
+            "/api/channels"
+        ]);
+    }
+    
+    function _initHttpServer(p_callback)
+    {
+        http.createServer(me.expressApp).listen(me.expressApp.get("port"), function()
+        {
+            mx.logger.info("HTTP server start to listen at " + me.expressApp.get("port") + ".");
+            if (isFunction(p_callback))
+            {
+                p_callback();
+            }
+        });
+    }
+    
+    
+    
+    
+    function _setStatus(p_status)
+    {
+        mx.logger.info("FeedStore server status is now switch to <%s>", p_status.toUpperCase());
+        me.status = p_status;
+    }
+    
+    
+    
+    
+    
+    function _printHeader()
+    {
+        console.log($format("\n", 25));
+        console.log($format(new Date(), "yyyy-MM-dd HH:mm:ss"));
+        console.log("***************************************************************************");
+        console.log(" MagicCube FeedStore 3.0 (%s@%s)", me.runningMode.toUpperCase(), me.runat.toUpperCase());
+        console.log("***************************************************************************");
+    }
+    
+    function _loadSettings()
+    {
+        
+    }
     
     return me.endOfClass(arguments);
 };
